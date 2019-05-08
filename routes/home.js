1371506fb032f1636ec533b96163818109ee6945
Reece Emero <reece.emero98@gmail.com>
2019-05-08 15:45:13 -0400

    Added authorization to homepage

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,25 @@
 const express = require("express")
 const router = express.Router()
+const User = require("../models/user")
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
+  // Authorization
+  if (!req.session.userId) {
+    const err = new Error("You are not authorized to view this page.")
+    err.status = 403
+    next(err)
+  }
   // Load data from DB
-  res.render("home", {
-    title: "Player's Hut"
+  User.findById(req.session.userId).exec((error, user) => {
+    if (error) {
+      next(error)
+    } else {
+      res.render("home", {
+        title: "Player's Hut",
+        username: user.username
+      })
+    }
   })
 })
 
